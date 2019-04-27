a63398f4bbba64104d0592eb6f4b948050b0712d
Takafumi Arakaki <aka.tkf@gmail.com>
2019-04-26 18:35:25 -0700

    Implement IndirectImports.jl

--- src/IndirectImports.jl
+++ src/IndirectImports.jl
@@ -1,5 +1,56 @@
 module IndirectImports
 
-greet() = print("Hello World!")
+export @indirect
+
+using MacroTools
+using UUIDs
+
+struct IndirectFunction{pkg, name}
+end
+
+struct IndirectPackage{uuid, pkgname}
+end
+
+IndirectPackage(uuid::UUID, pkgname::Symbol) = IndirectPackage{uuid, pkgname}()
+IndirectPackage(pkg::Base.PkgId) = IndirectPackage(pkg.uuid, Symbol(pkg.name))
+
+function IndirectPackage(mod::Module)
+    if parentmodule(mod) !== mod
+        error("Only the top-level module can be indirectly imported.")
+    end
+    return IndirectPackage(Base.PkgId(mod))
+end
+
+Base.getproperty(pkg::IndirectPackage, name::Symbol) =
+    IndirectFunction{pkg, name}
+
+function indirectfunction(pkgish, name::Symbol)
+    return IndirectFunction{IndirectPackage(pkgish), name}
+end
+
+
+"""
+    @indirect import Module=UUID
+
+Define an indirectly imported `Module`.
+
+    @indirect function interface_function end
+
+Define an `interface_function` which can be used and extended in downstream
+packages (via `@indirect import Module=UUID`) without loading the package
+defining `interface_function`.
+"""
+macro indirect(expr)
+    if @capture(expr, import name_=uuid_)
+        return esc(:(const $name = $(IndirectPackage(UUID(uuid), name))))
+    elseif @capture(expr, function name_ end)
+        return esc(:(const $name = $(indirectfunction(__module__, name))))
+    else
+        error("""
+        Cannot handle:
+        $expr
+        """)
+    end
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,25 @@
+module TestIndirectImports
+
 using IndirectImports
 using Test
+using UUIDs
+
+module Upstream
+    # using IndirectImports
+    # @indirect function fun end
+    using IndirectImports: indirectfunction
+    using UUIDs
+    const fun = indirectfunction(Base.PkgId(UUID("332e404b-d707-4859-b48f-328b8b3632c0"), "Upstream"), :fun)
+end
+
+module Downstream
+    using IndirectImports
+    @indirect import Upstream="332e404b-d707-4859-b48f-328b8b3632c0"
+    Upstream.fun(x) = x + 1
+end
 
 @testset "IndirectImports.jl" begin
-    # Write your own tests here.
+    @test Upstream.fun(1) == 2
 end
+
+end  # module
